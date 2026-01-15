855d3ecb9a7f0176388ca0045db7409b08619a90
AndreVasques <andrevasques@outlook.com.br>
2026-01-15 16:40:15 -0300

    First commit

---
+++ src/main/java/br/andrevasques/spring_exercises/SpringExercisesApplication.java
@@ -0,0 +1,13 @@
+package br.andrevasques.spring_exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExercisesApplication.class, args);
+	}
+
+}

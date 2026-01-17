7f481bdbc0b7d7d8c5c952511dc7db2cdd6fa7ba
AndreVasques <andrevasques@outlook.com.br>
2026-01-17 11:35:39 -0300

    Implemented ProductController with post method and persistency on MySQL db

---
+++ src/main/java/br/andrevasques/spring_exercises/controller/ProductController.java
@@ -0,0 +1,24 @@
+package br.andrevasques.spring_exercises.controller;
+
+import br.andrevasques.spring_exercises.model.entitites.Product;
+import br.andrevasques.spring_exercises.model.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @PostMapping
+    public Product newProduct(@RequestParam String name) {
+        Product product = new Product(name);
+        productRepository.save(product);
+        return product;
+    }
+}
--- src/main/java/br/andrevasques/spring_exercises/model/entitites/Product.java
+++ src/main/java/br/andrevasques/spring_exercises/model/entitites/Product.java
@@ -12,6 +12,10 @@ public class Product {
 
     private String name;
 
+    public Product(String name) {
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }

6add5f76dcdf65943745f695d715ab6d74eb90f7
AndreVasques <andrevasques@outlook.com.br>
2026-01-20 14:50:45 -0300

    Add WebConfig for stable JSON

---
+++ src/main/java/br/andrevasques/spring_exercises/config/WebConfig.java
@@ -0,0 +1,11 @@
+package br.andrevasques.spring_exercises.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(
+        pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
+public class WebConfig {
+}
